2381c450f0084dc0d80be129b6b760a9e9f577aa
Elenche Zetetique <elenche.zetetique@proton.me>
2025-04-07 12:46:29 +0200

    internal reference to the variable NODE_NAME in the helm release for Filebeat 'escaped' in order pass it through

--- remote/main.tf
+++ remote/main.tf
@@ -1015,7 +1015,7 @@ resource "helm_release" "filebeat" {
                   - /var/log/containers/*.log
                 processors:
                 - add_kubernetes_metadata:
-                    host: ${NODE_NAME}
+                    host: $\{NODE_NAME\}
                     matchers:
                     - logs_path:
                         logs_path: "/var/log/containers/"
